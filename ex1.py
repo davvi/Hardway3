d50605ff448a30174bf2787006e9697c8e26499f
Davii <linx82@gmail.com>
2016-02-25 23:12:38 +1100

    first excerise done, that was easy

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+print("Hello World")
+print("Hellow Again")
+print("I like typing this")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'")
+print('I "said" do not touch this')
+print("I can print another line of stuff")
+#print("I can comment out a line as well")
