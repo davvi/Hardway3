beca6dad76746f5e6cce76749efda391d5c49a6e
Davii <linx82@gmail.com>
2016-02-27 10:08:17 +1100

    Number 5 is done

---
+++ ex5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+#feel free to sub your own details.. like I have from the original of Mr Shaw's
+my_name = "Davii"
+my_age = 34 #not a lie!
+my_height = 186 #CM
+my_weight = 90 #kg .. I need to diet.
+my_eyes = "blue"
+my_teeth = "white" #maybe a little coffee staining
+my_hair = "Dark Brown"
+
+print("Let's talk about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("He's %d Kilo's heavy." %my_weight)
+print("Yeah I need to diet.")
+print("He's got %s eyes and %s hair." % (my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffee." % my_teeth)
+
+# this line is tricky, try to get it right
+print("If I add %d, %d, and %d, I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)) 
