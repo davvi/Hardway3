1ad856a205e67362b4f1f3bfffd00b5572151269
Davii <linx82@gmail.com>
2016-03-14 18:01:58 +1100

    More has been done..

---
+++ ex15.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print("Here's you file %r:" % filename)
+
+print(txt.read())
+
+print("Type the filename again:")
+
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
